d509801b6ac377f196eea8ea2297fc2a1e713bff
spencercolaco <spencercola@gmail.com>
2018-04-06 12:10:44 -0500

    Added comments

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+// Get canvas element and define context
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
+// Listens for keydown
 window.addEventListener("keydown", moveSomething, false);
 
-
+// Define player x,y and size
 var playerX = 75;
 var playerY = 75;
 var playerSize = 25;
 
+// Adds keycodes to the event listener and moves the circle
 function moveSomething(e) {
     switch(e.keyCode) {
         case 37:
         playerX -= 2;
-        console.log('w00t');
             // left key pressed
             break;
         case 38:
@@ -29,18 +31,23 @@ function moveSomething(e) {
             break;
     }
 
+    // Redraws the circle after key is pressed
     drawCircle();
 }
 
+// Renders the circle on the canvas
 function drawCircle() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  // Draws circle
   ctx.beginPath();
   ctx.arc(playerX, playerY, playerSize, 0, 2 * Math.PI);
   ctx.closePath();
 
+  // Colors the circle green
   ctx.fillStyle = 'green';
   ctx.fill();
 }
 
+// Renders initial circle 
 drawCircle();
