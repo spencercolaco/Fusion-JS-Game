e318866c7efd423d2e94d587f7802502f688b811
spencercolaco <spencercola@gmail.com>
2018-04-06 12:05:28 -0500

    Circle moves with arrow keys. Everything is working fine. Moving on to making it move faster.

--- app.js
+++ app.js
@@ -3,26 +3,28 @@ var ctx = canvas.getContext('2d');
 
 window.addEventListener("keydown", moveSomething, false);
 
-var deltaX = 0;
-var deltaY = 0;
+
+var playerX = 75;
+var playerY = 75;
+var playerSize = 25;
 
 function moveSomething(e) {
     switch(e.keyCode) {
         case 37:
-        deltaX -= 2;
+        playerX -= 2;
         console.log('w00t');
             // left key pressed
             break;
         case 38:
-        deltaY -= 2;
+        playerY -= 2;
             // up key pressed
             break;
         case 39:
-        deltaX += 2;
+        playerX += 2;
             // right key pressed
             break;
         case 40:
-        deltaY += 2;
+        playerY += 2;
             // down key pressed
             break;
     }
@@ -34,12 +36,11 @@ function drawCircle() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   ctx.beginPath();
+  ctx.arc(playerX, playerY, playerSize, 0, 2 * Math.PI);
+  ctx.closePath();
 
-  ctx.moveTo(200 + deltaX, 100 + deltaY);
-
-  ctx.arc(100,75,50,0,2*Math.PI);
   ctx.fillStyle = 'green';
-  ctx.fill()
+  ctx.fill();
 }
 
 drawCircle();
